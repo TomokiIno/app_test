89000ae90fcb62c0939f8dfa625a125ec3e99ee7
katarigod <98676007+katarigod@users.noreply.github.com>
2025-01-15 15:13:39 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import Button from '../components/Button';
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>カラーコードアプリケーション</h1>
+      <Button/>
+    </div>
+  );
+}
